b79c4495d61a28582ee5c02eec13966e4f546bd7
Jason Edelson <edelson.m.jason@gmail.com>
2024-03-10 00:22:43 -0800

    route handling added

--- src/components/ReviewComponent/CarouselComponent/CarouselComponent.jsx
+++ src/components/ReviewComponent/CarouselComponent/CarouselComponent.jsx
@@ -1,21 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import './CarouselComponent.css';
-
+import { useLocation, useNavigate } from 'react-router-dom';
 const CarouselComponent = ({ images, onImageChange }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [clicked, setClicked] = useState({ left: false, right: false });
-
+    let location = useLocation();
+    let navigate = useNavigate();
     useEffect(() => {
         if (onImageChange) {
             onImageChange(images[currentIndex].key);
         }
     }, [currentIndex, images, onImageChange]);
+    useEffect(() => {
+        let check = location.pathname.replace(/^\/+|\/+$/g, '');
+        for (let [index, item] of images.entries()) {
+            let oreoNameCheck = item.name.toLowerCase().replace(/\s+/g, '');
+            console.log(oreoNameCheck)
+            console.log(check)
+            console.log(oreoNameCheck === check)
+            if (oreoNameCheck === check) {
+                onImageChange(item.key);
+                setCurrentIndex(index)
+                console.log("match ==", item)
+                return
+            }
+        }
+    }, [])
 
     const goToPrevious = () => {
         setClicked({ ...clicked, left: true });
         setTimeout(() => setClicked({ ...clicked, left: false }), 100);
         const isFirstImage = currentIndex === 0;
         const newIndex = isFirstImage ? images.length - 1 : currentIndex - 1;
+        let name = images[newIndex].name.toLowerCase().replace(/\s+/g, '');
+        navigate(`/${name}`)
         setCurrentIndex(newIndex);
     };
 
@@ -24,6 +42,8 @@ const CarouselComponent = ({ images, onImageChange }) => {
         setTimeout(() => setClicked({ ...clicked, right: false }), 100);
         const isLastImage = currentIndex === images.length - 1;
         const newIndex = isLastImage ? 0 : currentIndex + 1;
+        let name = images[newIndex].name.toLowerCase().replace(/\s+/g, '');
+        navigate(`/${name}`)
         setCurrentIndex(newIndex);
     };
 
